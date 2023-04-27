17a1b79dd2dc0ce7b2dfaba7938b42908a6e16a7
melqui16rv <120046235+melqui16rv@users.noreply.github.com>
2023-04-27 16:00:13 -0500

    corrección del trabajo dos primera

--- Repositorio/Segundo trimestre/trabajos/trab2/miFor.py
+++ Repositorio/Segundo trimestre/trabajos/trab2/miFor.py
@@ -1,4 +1,4 @@
-fin=int(input("Digite un limite de la serie: "))
+fin=int(input("Digite un hasta donde quiere que vata la serie: "))
 cantidad=int(input("Digite la cantidad de cuanto va a ir incrementando o decrementando: "))
 if cantidad>0:
     serie=range(0, fin+1, cantidad)
@@ -6,8 +6,8 @@ else:
     serie=range(fin, -1, cantidad)
 print("Serie generada:")
 for num in serie:
-    print(num, end=' ')
-n = int(input("\nIntroduce un número positivo para contar sus múltiplos: "))
+    print(num,)
+n=int(input("Introduce un numero positivo: "))
 while n<=0:
     n=int(input("El número debe ser positivo, introduce uno nuevo: "))
 multiplos=0
