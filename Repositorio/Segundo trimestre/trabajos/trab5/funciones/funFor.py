520203d76a014c73f9c014c99e5c6a2c2039d120
melqui16rv <120046235+melqui16rv@users.noreply.github.com>
2023-05-09 19:14:18 -0500

    Subi el trabajo 5 y agrege las misiones faltantes del trabajo 5

--- Repositorio/Segundo trimestre/trabajos/trab5/funciones/funFor.py
+++ Repositorio/Segundo trimestre/trabajos/trab5/funciones/funFor.py
@@ -1,16 +1,16 @@
 """Solicitar 2 números al usuario e imprimir el cociente y el
 residuo del mayor en el menor sin utilizar la división ni el mod. """
+
 def obtener_cociente_residuo(num1, num2):
-    mayor = max(num1, num2)
-    menor = min(num1, num2)
-    cociente = mayor // menor
-    residuo = mayor % menor
-    return (cociente, residuo)
-
-num1 = int(input("Ingrese el primer número: "))
-num2 = int(input("Ingrese el segundo número: "))
-
-cociente, residuo = obtener_cociente_residuo(num1, num2)
-
-print(f"El cociente del mayor en el menor es: {cociente}")
-print(f"El residuo  del mayor en el menor es: {residuo}")
+    mayor=max(num1, num2)
+    menor=min(num1, num2)
+    cociente=mayor//menor
+    residuo=mayor % menor
+    return(cociente, residuo)
+num_repeticiones=int(input("Ingrese el número de repeticiones: "))
+for i in range(num_repeticiones):
+    num1=int(input("Ingrese el primer número: "))
+    num2=int(input("Ingrese el segundo número: "))
+    cociente, residuo=obtener_cociente_residuo(num1, num2)
+    print(f"El cociente del mayor en el menor es: {cociente}")
+    print(f"El residuo  del mayor en el menor es: {residuo}")
